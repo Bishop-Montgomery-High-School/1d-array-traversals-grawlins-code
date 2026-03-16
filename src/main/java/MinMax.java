4f47825ec7ebaad8dde7fb04e3427f406885b1b1
grawlins <grawlins@student-bmhs-la.org>
2026-03-16 13:57:35 -0700

    counter class compleated working

--- src/main/java/MinMax.java
+++ src/main/java/MinMax.java
@@ -15,9 +15,15 @@ public class MinMax {
      */
     public int min(int[] arr) {
         // TODO: initialize a tracking variable to arr[0]
+        int tracker = arr[0];
 
         // TODO: loop from index 1 to arr.length - 1
         //       if arr[i] is less than your tracking variable, update it
+        for(int i=1; i< arr.length-1; i++) {
+            if(arr[i]<tracker) {
+                tracker = arr[i];
+            }
+        }
 
         // TODO: return the tracking variable
         return 0; // placeholder
